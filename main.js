4d67a1a25e4b9006edc66fb7fa5fdb234f48b111
ryan <ryandehallo229@gmail.com>
2021-09-18 22:08:56 -0300

    Comunicação com o banco de dados

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 const express = require('express')
+const database = require('./services/database/db')
 const app = express()
+
 
 const dirname = __dirname
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+
+app.use(express.urlencoded({ 
+    extended: true 
+    }
+))
 app.use(express.static(dirname + '/public'))
 
+async function verifyConnection(){
+    try{
+        await database.authenticate();
+        console.log('authenticated...');
+        const User = require('./services/database/modelUser/user')
+
+        await database.sync()
+
+        const newUser = await User.create({
+            email: "mika1394@gmail.com",
+            senha: "13294mikaela"
+        })
+
+        //const users = await User.findAll()
+        console.log(newUser);
+
+        
+    }catch(err){
+        console.error("Not connected", err);
+    }
+}
 
 app.get('/login', function(req, res) {
     res.sendFile(dirname + '/public/pages/login_page/login.html')
 
 })
-    
+
 
 app.post("/clients", function(req, res){
     async function getData(res){
@@ -29,4 +56,5 @@ app.post("/clients", function(req, res){
 
 app.listen(3000, function() {
     console.log("Listening on http://localhost:3000\n")
+    verifyConnection()
 })
